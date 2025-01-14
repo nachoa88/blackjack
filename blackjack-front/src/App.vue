d5312c2b87bd8b65ab310ba61f42f922b548a761
nacho <nacho.albiol88@gmail.com>
2025-01-14 13:13:09 +0100

    Improve layout structure

--- blackjack-front/src/App.vue
+++ blackjack-front/src/App.vue
@@ -1,18 +1,72 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import { ref } from "vue";
+
+const showDropdown = ref(false);
+const toggleDropdown = () => {
+  showDropdown.value = !showDropdown.value;
+};
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col bg-slate-400">
     <!-- Navbar -->
     <header>
-      <nav class="flex justify-center gap-6 p-4 m-4 bg-slate-600 rounded-lg shadow-lg">
-        <div class="text-slate-200">Logo Here!</div>
-        <RouterLink to="/" class="nav-link">Home</RouterLink>
-        <RouterLink to="/ranking" class="nav-link">Ranking</RouterLink>
-        <RouterLink to="/players" class="nav-link">Players</RouterLink>
-        <!-- Login / Register / Logut -->
-      </nav>
+      <div class="flex justify-center p-4 m-4 bg-slate-600 rounded-lg shadow-lg">
+        <!-- Logo -->
+        <div class="flex items-center text-slate-200 px-6">
+          <a href="/"> Logo Here! </a>
+        </div>
+        <!-- Navigation Links -->
+        <div class="flex items-center gap-6">
+          <RouterLink to="/" class="inline-flex items-center nav-link nav-link-hover">Home</RouterLink>
+          <RouterLink to="/ranking" class="inline-flex items-center nav-link nav-link-hover">Rankings</RouterLink>
+          <RouterLink to="/players" class="inline-flex items-center nav-link nav-link-hover">Players</RouterLink>
+
+          <!-- Authenticated user dropdown -->
+          <!-- <div class="relative">
+            <button
+              type="button"
+              @click="toggleDropdown"
+              class="inline-flex items-center nav-link bg-teal-700 hover:bg-teal-800 rounded-lg px-4 py-2.5 gap-1"
+            >
+              Nickname
+              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <div
+              v-if="showDropdown"
+              class="absolute right-0 mt-2 bg-white rounded shadow-lg py-2 text-sm text-slate-700"
+            >
+              <a href="#" class="block px-4 py-2 hover:bg-gray-100">Profile</a>
+              <a href="#" class="block px-4 py-2 hover:bg-gray-100">Logout</a>
+            </div>
+          </div> -->
+
+          <!-- Guest user login and register -->
+          <div class="flex gap-4">
+            <RouterLink
+              to="/login"
+              class="inline-flex items-center nav-link bg-teal-600 hover:bg-teal-700 rounded-lg px-4 py-2"
+            >
+              Log in
+            </RouterLink>
+            <RouterLink
+              to="/register"
+              class="inline-flex items-center nav-link bg-teal-600 hover:bg-teal-700 rounded-lg px-4 py-2"
+            >
+              Register
+            </RouterLink>
+          </div>
+        </div>
+
+        <!-- Hamburger Menu -->
+      </div>
     </header>
 
     <!-- Main Content -->
@@ -34,14 +88,4 @@ import { RouterLink, RouterView } from "vue-router";
   </div>
 </template>
 
-<style scoped>
-.nav-link {
-  color: whitesmoke;
-  text-decoration: none;
-  text-transform: uppercase;
-  font-weight: 500;
-}
-.nav-link:hover {
-  text-decoration: underline;
-}
-</style>
+<style scoped></style>
--- blackjack-front/src/router/index.js
+++ blackjack-front/src/router/index.js
@@ -18,6 +18,16 @@ const router = createRouter({
       path: "/players",
       name: "players",
       component: () => import("../views/PlayersView.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
     }
   ],
 })
